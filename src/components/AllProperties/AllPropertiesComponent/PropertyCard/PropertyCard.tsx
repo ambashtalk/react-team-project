52fe4a38edcf4837ff0b7fc25c03fc0ce4b8915e
Lohitya <ambashtalk@gmail.com>
2021-02-22 13:20:11 +0530

    Restructure folder structure and formatted code

--- src/components/AllProperties/AllPropertiesComponent/PropertyCard/PropertyCard.tsx
+++ src/components/AllProperties/AllPropertiesComponent/PropertyCard/PropertyCard.tsx
@@ -1,97 +1,93 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
-import DeletePropertyButton from '../DeletePropertyButton/DeletePropertyButton'
-import {PropertyDetails,CardHolder,UniquePropertyDetails,PropertyImageStyes,ImageWithDetailsStyles,CardContainer} from './PropertyCardStyles'
-import axios, { AxiosResponse } from 'axios'
-import { FloatButtonArea } from '../../../saurabhForm/FormComponents'
-
-
+import React, { useState } from "react";
+import DeletePropertyButton from "../DeletePropertyButton/DeletePropertyButton";
+import {
+  PropertyDetails,
+  CardHolder,
+  UniquePropertyDetails,
+  PropertyImageStyes,
+  ImageWithDetailsStyles,
+  CardContainer,
+} from "./PropertyCardStyles";
+import { FloatButtonArea } from "../../../SignUpForm/SignUpFormComponents";
 
 type CardProps = {
-    id:string,
-    nameOfProperty:string,
-    locationOfProperty:string,
-    costOfProperty:string,
-    activeUser:string,
-    
-    typeOfProperty:string,
-    descriptionOfProperty:string,
-    ownerOfProperty:string,
-    registrationDateOfProperty:string,
-    removePropertyHandler:(id:string) => void
+  id: string;
+  nameOfProperty: string;
+  locationOfProperty: string;
+  costOfProperty: string;
+  activeUser: string;
+
+  typeOfProperty: string;
+  descriptionOfProperty: string;
+  ownerOfProperty: string;
+  registrationDateOfProperty: string;
+  removePropertyHandler: (id: string) => void;
+};
+
+const PropertyCard = (props: CardProps) => {
+  let [fullPropertyDetailsToggler, setfullPropertyDetailsToggler] = useState(
+    false
+  );
+
+  function givefullviewofproperty() {
+    if (fullPropertyDetailsToggler == false) {
+      setfullPropertyDetailsToggler(true);
+    } else {
+      setfullPropertyDetailsToggler(false);
+    }
   }
 
+  let imagesource = "https://picsum.photos/450/450?random=" + props.id;
 
-  
+  return (
+    <CardContainer onClick={givefullviewofproperty}>
+      <CardHolder>
+        <ImageWithDetailsStyles>
+          <PropertyDetails>
+            <PropertyImageStyes src={imagesource}></PropertyImageStyes>
+            <UniquePropertyDetails>
+              Name:{props.nameOfProperty}
+            </UniquePropertyDetails>
+            <UniquePropertyDetails>
+              Location:{props.locationOfProperty}
+            </UniquePropertyDetails>
+            <UniquePropertyDetails>
+              Cost:{props.costOfProperty}
+            </UniquePropertyDetails>
 
-
-const PropertyCard=(props:CardProps)=>{
-
-
-    let [fullPropertyDetailsToggler,setfullPropertyDetailsToggler]=useState(false);
-       
-    function givefullviewofproperty(){
-        if(fullPropertyDetailsToggler==false){
-            setfullPropertyDetailsToggler(true);}
-        else{
-            setfullPropertyDetailsToggler(false);
-
-
-        }
-    }
-
-    let imagesource="https://picsum.photos/450/450?random=" + props.id;
-
-
-
-
-        return (
-            <CardContainer onClick={givefullviewofproperty} >
-                
-                
-                <CardHolder>
-                    <ImageWithDetailsStyles>
-                    
-                
-                   
-                        <PropertyDetails>
-                            <PropertyImageStyes src={imagesource}></PropertyImageStyes>
-                            <UniquePropertyDetails>
-                            
-            
-                                Name:{props.nameOfProperty}
-                            </UniquePropertyDetails>
-                            <UniquePropertyDetails >
-                            
-                                Location:{props.locationOfProperty}
-                            </UniquePropertyDetails>
-                            <UniquePropertyDetails >
-                        
-                                Cost:{props.costOfProperty}
-                            </UniquePropertyDetails>
-
-                            {fullPropertyDetailsToggler? <UniquePropertyDetails >Property Type:{props.typeOfProperty}</UniquePropertyDetails>:null}
-                            {fullPropertyDetailsToggler? <UniquePropertyDetails >Description:{props.descriptionOfProperty}</UniquePropertyDetails>:null}
-                            {fullPropertyDetailsToggler? <UniquePropertyDetails >Owner:{props.ownerOfProperty}</UniquePropertyDetails>:null}
-                            {fullPropertyDetailsToggler? <UniquePropertyDetails >Registration Date:{props.registrationDateOfProperty}</UniquePropertyDetails>:null}
-                            
-
-                
-        
-        
-                         </PropertyDetails>
-                        <div>
-                            {props.activeUser==props.ownerOfProperty?<DeletePropertyButton removePropertyHandler={props.removePropertyHandler}/>:null}
-                        </div>
-                    </ImageWithDetailsStyles>
-                </CardHolder>
-                <FloatButtonArea />
-            </CardContainer>
-            
-        )
-            
-
-    }
-
+            {fullPropertyDetailsToggler ? (
+              <UniquePropertyDetails>
+                Property Type:{props.typeOfProperty}
+              </UniquePropertyDetails>
+            ) : null}
+            {fullPropertyDetailsToggler ? (
+              <UniquePropertyDetails>
+                Description:{props.descriptionOfProperty}
+              </UniquePropertyDetails>
+            ) : null}
+            {fullPropertyDetailsToggler ? (
+              <UniquePropertyDetails>
+                Owner:{props.ownerOfProperty}
+              </UniquePropertyDetails>
+            ) : null}
+            {fullPropertyDetailsToggler ? (
+              <UniquePropertyDetails>
+                Registration Date:{props.registrationDateOfProperty}
+              </UniquePropertyDetails>
+            ) : null}
+          </PropertyDetails>
+          <div>
+            {props.activeUser == props.ownerOfProperty ? (
+              <DeletePropertyButton
+                removePropertyHandler={props.removePropertyHandler}
+              />
+            ) : null}
+          </div>
+        </ImageWithDetailsStyles>
+      </CardHolder>
+      <FloatButtonArea />
+    </CardContainer>
+  );
+};
 
 export default PropertyCard;
---
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -0,0 +1,88 @@
+import React, { FunctionComponent } from "react";
+import {
+  ButtonArea,
+  DivArea,
+  FormArea,
+  InputArea,
+} from "./SignUpFormComponents";
+
+export const Signup: FunctionComponent = () => {
+  return (
+    <React.Fragment>
+      <FormArea>
+        <InputArea
+          className="TypeA"
+          type="text"
+          placeholder="Company Name"
+          label="Company Name"
+          name="cName"
+        ></InputArea>
+
+        <DivArea className="TypeB">
+          <InputArea
+            className="TypeC"
+            type="text"
+            placeholder="Title"
+            label="Title"
+            name="title"
+          ></InputArea>
+          <InputArea
+            className="TypeD"
+            type="text"
+            placeholder="First Name"
+            label="First Name"
+            name="fName"
+          ></InputArea>
+          <InputArea
+            className="TypeD"
+            type="text"
+            placeholder="Last Name"
+            label="Last Name"
+            name="lName"
+          ></InputArea>
+        </DivArea>
+
+        <InputArea
+          className="TypeA"
+          type="text"
+          placeholder="Email Address"
+          label="Email"
+          name="email"
+        ></InputArea>
+
+        <DivArea className="TypeB">
+          <InputArea
+            className="TypeC"
+            type="date"
+            placeholder="Pin Code"
+            label="Pin Code"
+            name="pCode"
+          ></InputArea>
+          <InputArea
+            className="TypeD"
+            type="text"
+            placeholder="Mobile Number"
+            label="Mobile"
+            name="mobile"
+          ></InputArea>
+        </DivArea>
+
+        <InputArea
+          className="TypeA"
+          type="text"
+          placeholder="Password"
+          label="Password"
+          name="pass"
+        ></InputArea>
+        <InputArea
+          className="TypeA"
+          type="text"
+          placeholder="Confirm Password"
+          label="Confirm Password"
+          name="cPass"
+        ></InputArea>
+      </FormArea>
+      <ButtonArea className="TypeE" message="Submit"></ButtonArea>
+    </React.Fragment>
+  );
+};
